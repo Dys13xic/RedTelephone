1e3fc06954fe0a23110b67df46ebd18767d8a9ff
Dys13xic <campbell.leckie@hotmail.com>
2024-10-18 02:46:34 -0400

    Began implementing timers for Invite request. Removed Send thread and queue in favour of iterative function. Minor tweaks

--- sip.py
+++ sip.py
@@ -9,6 +9,7 @@ import queue
 PORT = 5060
 READ_SIZE = 2048
 SOCKET_TIMEOUT = 1
+T1 = 0.5 # Estimate of RTT (default 500 milliseconds)
 
 def _getHeader(headers, targetLabel):
     for header in headers:
@@ -25,21 +26,24 @@ def _getHeader(headers, targetLabel):
 # The sender and listener methods are sort of both catered specifically to SIP traffic...
 class UDPHandler:
 
-    def __init__(self, port, sendQueue, recvQueue, halt):
+    def __init__(self, port, recvQueue, halt):
         self.localPort = port
         self.localIP = None
-        self.sendQueue = sendQueue
         self.recvQueue = recvQueue
         self.halt = halt
+        # self.sendSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def listener(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(SOCKET_TIMEOUT)
         sock.bind(("", self.localPort))
 
+        # Retrieve local IP
         try:
-            sock.connect(("8.8.8.8", 80))
-            self.localIP = sock.getsockname()[0]
+            tempSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            tempSock.connect(("8.8.8.8", 80))
+            self.localIP = tempSock.getsockname()[0]
+            tempSock.close()
         except:
             "Failed to determine local IP address"
             exit()
@@ -83,15 +87,9 @@ class UDPHandler:
 
         sock.close()
 
-    def sender(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        while(self.halt.is_set() == False):
-            try:
-                (targetAddress, targetPort, data) = self.sendQueue.get(timeout=1)
-                bytesSent = sock.sendto(data, (targetAddress, targetPort))
-                # TODO confirm that bytes sent matches data size (is this necessary? UDP packets are atomic, I suppose we should still check that data has gone out and log otherwise)
-            except queue.Empty:
-                continue
+    def sender(self, target, message):
+        bytesSent = self.sendSock.sendto(message, target)
+        # TODO confirm that bytes sent matches data size (is this necessary? UDP packets are atomic, I suppose we should still check that data has gone out and log otherwise)
 
     def getLocalAddress(self):
         return (self.localIP, self.localPort)
@@ -117,8 +115,8 @@ class UDPHandler:
 #         #self.routeSet = routeSet
 
 class Transaction:
-
-    def __init__(self, localAddress, remoteAddres, state, dialog):
+    def __init__(self, transactionUser, localAddress, remoteAddres, state, dialog):
+        self.transactionUser = transactionUser
         self.localIP, self.localPort = localAddress
         self.remoteIP, self.remotePort = remoteAddres
         self.state = state
@@ -136,7 +134,7 @@ class ClientTransaction(Transaction):
             self.fromTag = dialog.getLocalTag()
             self.toTag = dialog.getRemoteTag()
             self.callID = dialog.getCallID()
-            self.sequence = #TODO is it remote or local Seq #?
+            self.sequence = dialog.getLocalSeq() + 1        # TODO update dialog settings after request sent
         
         else:
             self.fromTag = hex(int(random.getrandbits(32)))[2:]
@@ -144,28 +142,45 @@ class ClientTransaction(Transaction):
             self.sequence = 1
 
         self.branch = Sip.BRANCH_MAGIC_COOKIE + hashlib.md5((self.toTag + self.fromTag + self.callID + "SIP/2.0/UDP {}:{};".format(self.localIP, self.localPort) + str(self.sequence)).encode()).hexdigest()
-
+        self.transactionUser.addClientTransaction(self)
 
     def buildRequest(self, method):
         if(method=="INVITE"):
-            Sip._buildMessage("INVITE", (self.localIP, self.localPort), (self.remoteIP, self.remotePort), )
+            Sip._buildMessage("INVITE", (self.localIP, self.localPort), (self.remoteIP, self.remotePort), self.branch, self.callID, self.sequence, )
 
-        elif(method=="CANCEL"):
+        # elif(method=="CANCEL"):
 
-        elif(method=="BYE"):
+        # elif(method=="BYE"):
             
-        elif(method=="ACK"):
+        # elif(method=="ACK"):
 
         else:
             #TODO better error handling here
             print("Unsupported Request Method")
             exit()
 
-    def invite():
+    # def transmit(self, attempts=1):
+
+
+
+
+    def invite(self):
         # Send Invite
         request = ClientTransaction.buildRequest("INVITE")
 
         # Initate timers
+        timeoutTimer = threading.Timer(64 * T1) # If still in calling state when timeoutTimer triggers, inform TU of timeout
+        
+        self.transactionUser.getUDPHandler()
+        while self.state == "Calling" and timeoutTimer.is_alive():
+                retransmitTimer = threading.Timer(pow(2, attempt) * T1) # If fires, client transaction must retransmit request and reset timer
+                attempt += 1
+
+
+
+
+
+
 
         # Await 1xx response
 
@@ -174,13 +189,13 @@ class ClientTransaction(Transaction):
         # Create a seperate Transaction for Acking
 
 
-    def nonInvite(method):
+    # def nonInvite(method):
 
-class ServerTransaction(Transaction):
-    def __init__(self, localAddress, remoteAddress, state, sequence=1, callID)
+# class ServerTransaction(Transaction):
+#     def __init__(self, localAddress, remoteAddress, state, sequence=1, callID)
         
 
-    def buildResponse(status):
+#     def buildResponse(status):
 
 
 
@@ -192,20 +207,24 @@ class Sip:
 
     def __init__(self, port=5060):
         self.port = port
-        self.transactions = []
+        self.clientTransactions = {}
         self.dialogs = []
 
-        self.sendQueue = queue.Queue()
         self.recvQueue = queue.Queue()
         self.halt = threading.Event()
-        self.UDPHandler = UDPHandler(self.port, self.sendQueue, self.recvQueue, self.halt)
+        self.UDPHandler = UDPHandler(self.port, self.recvQueue, self.halt)
 
         self.UDPListenerThread = threading.Thread(target=self.UDPHandler.listener)
-        self.UDPSenderThread = threading.Thread(target=self.UDPHandler.sender)
         self.SIPHandlerThread = threading.Thread(target=self.handler)
 
+    def getUDPHandler(self):
+        return self.UDPHandler
+
+    def addClientTransaction(self, transaction):
+        self.clientTransactions[transaction.getBranch()] = transaction
+
     @staticmethod
-    def _buildMessage(type, localAddress, remoteAddress, branch=None, fromTag="", toTag="", callID=None, sequence=None, sequenceRequestType ="", messageBody=""):
+    def _buildMessage(type, localAddress, remoteAddress, branch, callID, sequence, sequenceRequestType, fromTag, toTag="", messageBody=""):
 
         # TODO
         #  When the server transport receives a request over any transport, it
@@ -217,36 +236,19 @@ class Sip:
         (localIP, localPort) = localAddress
         (remoteIP, remotePort) = remoteAddress
 
-        # TODO may need to handle ;received
-        if(not callID and type == "INVITE"):
-            callID = hex(time.time_ns())[2:] + hex(int(random.getrandbits(32)))[2:]
-        else:
-            print("Failed to include Call-ID")
-            exit()            
-            
-        headers = {}
-        headers["Call-ID"] = callID
+        # TODO may need to handle ;received            
+        headers = {"Call-ID": callID}
 
         if(type in ["INVITE", "ACK", "CANCLE", "BYE"]):            
             startLine = "{} SIP:{}:{} SIP/2.0\r\n".format(type, remoteIP, remotePort)
-
-            sequenceRequestType = type
-            if(not sequence):
-                sequence = "1"
-
             headers["Via"] = "SIP/2.0/UDP {}:{}".format(localIP, localPort)
             headers["From"] = "<sip:IPCall@{}:{}>".format(localIP, localPort, fromTag)
             headers["To"] = "<sip:{}:{}>".format(remoteIP, remotePort, toTag)
             headers["Max-Forwards"] = "70"
-
+            sequenceRequestType = type
 
         elif(type in ["100 Trying", "180 Ringing", "200 OK", "400 Bad Request", "408 Request Timeout", "486 Busy Here", "487 Request Terminated"]):
-            startLine = "SIP/2.0 {}\r\n".format(type)
-
-            if (not sequence or not sequenceRequestType):
-                print("Failed to include CSeq")
-                exit()
-            
+            startLine = "SIP/2.0 {}\r\n".format(type)            
             headers["Via"] = "SIP/2.0/UDP {}:{}".format(remoteIP, remotePort)
             headers["From"] = "<sip:IPCall@{}:{}>".format(remoteIP, remotePort, fromTag),
             headers["To"] = "<sip:{}:{}>".format(localIP, localPort, toTag)
@@ -254,56 +256,51 @@ class Sip:
         else:
             print("{} Not implemented".format(type))
             exit()
-
-        if(not branch):
-            branch = Sip.BRANCH_MAGIC_COOKIE + hashlib.md5((toTag + fromTag + headers["Call-ID"] + headers["Via"] + str(sequence)).encode()).hexdigest()
         
         headers["Via"] += ";branch=" + branch
         headers["CSeq"] = "{} {}".format(sequence, sequenceRequestType)
         headers["Content-Length"] = str(len(messageBody.encode("utf-8")))
 
+        # Build message
         message = startLine
         for key in headers.keys():
             message += key + ": " + headers[key] + "\r\n"
 
         message += "\r\n{}".format(messageBody)
-
         return message.encode("utf-8")
 
 
     def start(self):
         self.SIPHandlerThread.start()
         self.UDPListenerThread.start()
-        self.UDPSenderThread.start()
         print("SIP service started on port: {}\n".format(self.port))
 
     def stop(self):
         self.halt.set()
         self.SIPHandlerThread.join()
         self.UDPListenerThread.join()
-        self.UDPSenderThread.join()
         print("SIP service terminated")
 
     def invite(self, address, port):
         print("Attempting to intitate a call with {}:{}".format(address, port))
         # TODO send invite request to handler
         inviteRequest = SIPService._buildMessage("INVITE", ("10.13.0.23", 5060), (address, port))
-        self.sendQueue.put((address, port, inviteRequest))
+        self.UDPHandler.sender(inviteRequest, (address, port))
 
-    def cancel(self, address, port):
-        print("Call cancelled")
-        # TODO send cancel request to handler
+    # def cancel(self, address, port):
+    #     print("Call cancelled")
+    #     # TODO send cancel request to handler
 
-    def bye(self, address, port):
-        print("Call ended")
-        # TODO send bye request to handler
+    # def bye(self, address, port):
+    #     print("Call ended")
+    #     # TODO send bye request to handler
 
     def handler(self):
         while(self.halt.is_set() == False):
             try:
                 (targetAddress, targetPort, data) = self.recvQueue.get(timeout=1)
 
-                # TODO deconstruct data
+                print(data)
 
 
 
@@ -312,17 +309,18 @@ class Sip:
                 continue
         
 SIPService = Sip()
-SIPService.start()
+SIPService.getUDPHandler().listener()
+# SIPService.start()
 # SIPService.invite("10.13.0.6", 5060)
 
-command = input()
-while(command != "EXIT"):
-    if(command == "INVITE"):
-        SIPService.invite("Remotehost", 5060)
-    elif(command == "CANCEL"):
-        SIPService.cancel("Remotehost", 5060)
-    elif(command == "BYE"):
-        SIPService.bye("Remotehost", 5060)
-    command = input()
+# command = input()
+# while(command != "EXIT"):
+#     if(command == "INVITE"):
+#         SIPService.invite("Remotehost", 5060)
+#     elif(command == "CANCEL"):
+#         SIPService.cancel("Remotehost", 5060)
+#     elif(command == "BYE"):
+#         SIPService.bye("Remotehost", 5060)
+#     command = input()
 
-SIPService.stop()
+# SIPService.stop()
