7667eba7ef20d5b8ae9080859600401b92a63b24
Dys13xic <campbell.leckie@hotmail.com>
2024-11-19 02:05:18 -0500

    Implemented GatewayMessage dataclass and json str to class obj conversion method

--- gateway.py
+++ gateway.py
@@ -4,13 +4,39 @@ import websockets
 # Standard library
 import asyncio
 import json
+from dataclasses import dataclass
+from types import SimpleNamespace
 
 URI = "wss://gateway.discord.gg"
 API_VERSION = 10
 
-class Gateway():
+@dataclass
+class GatewayMessage:
+    op: int
+    d: dict = None
+    s: int = None
+    t: int = None
 
-    def __init__(self):
+    def stringify(self):
+        pass
+
+    @staticmethod
+    def objectify(string):
+        jsonDict = json.loads(string)
+        classObj = GatewayMessage(**jsonDict)
+        # Convert data field to a Simple Namespace
+        # print(classObj.d)
+        # classObj.d = json.loads(classObj.d, object_hook=lambda data: SimpleNamespace(**data))
+        # return classObj
+
+class Gateway:
+    token: str
+    _heartbeatInterval: float
+    _lastSequence: int = None
+    _sendQueue: asyncio.Queue
+
+    def __init__(self, token):
+        self.token = token
         self._heartbeatInterval = 1
         self._lastSequence = None
         self._sendQueue = asyncio.Queue()
@@ -21,6 +47,7 @@ class Gateway():
             self._recvTask = asyncio.create_task(self._msgRecv(websocket))
             self._sendTask = asyncio.create_task(self._msgSend(websocket))
             self._heartbeatTask = asyncio.create_task(self._heartbeat())
+            # asyncio.gather(self._msgRecv(websocket), self._msgSend(websocket), self._heartbeat())
             await self._recvTask
             await self._heartbeatTask
             await self._sendTask
@@ -33,7 +60,7 @@ class Gateway():
     async def _heartbeat(self):
         while True:
             lastSequence = self._lastSequence if self._lastSequence else "null"
-            msg = {"op": 11, "d": lastSequence}
+            msg = {"op": 1, "d": lastSequence}
             await self._sendQueue.put(json.dumps(msg))
             await asyncio.sleep(self._heartbeatInterval)
 
@@ -53,12 +80,23 @@ class Gateway():
             try:
                 async for msg in websocket:
                     print(msg)
-                    #await process(msg)
+                    msgObj = GatewayMessage.objectify(msg)
+                    await self.processMsg(msgObj)
             except websockets.exceptions.ConnectionClosed as e:
                 print(e)
                 await self._stop()
 
+    async def processMsg(msgObj):
+        raise NotImplementedError
+
+    def setHeartbeatInterval(self, ms):
+        self._heartbeatInterval = ms / 1000
+
+    # def setLastSequence(self, sequence):
+    #     self._lastSequence = sequence
+
 g = Gateway()
+# asyncio.run(g._run())
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 try:
