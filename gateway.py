da17ef28e2b8b97e805fcbaaab0ab6a328166a3b
Dys13xic <campbell.leckie@hotmail.com>
2024-11-20 01:54:20 -0500

    Implemented event handler registration

--- gateway.py
+++ gateway.py
@@ -1,18 +1,38 @@
-from gateway import Gateway, GatewayMessage
+import asyncio
+import sys
 
-class GatewaySession(Gateway):
+from gateway_connection import GatewayConnection, GatewayMessage
+
+class Gateway(GatewayConnection):
+    _eventListeners: dict = {}
+
     def __init__(self, token):
         super().__init__(token)
+
+    def eventHandler(self, func):
+        self._eventListeners[func.__name__] = func
+        return func
 
     async def processMsg(self, msgObj):
         match msgObj.op:
             # Event Dispatched
             case 0:
-                pass
+                # Update sequence number
+                if(msgObj.s):
+                    self.setLastSequence(msgObj.s)
+
+                if(msgObj.t == "READY"):
+                    # TODO grab info for resuming session
+                    pass
+                # Pass to relevant event handler
+                elif(msgObj.t.lower() in self._eventListeners.keys()):
+                    self._eventListeners[msgObj.t.lower()](msgObj)
 
             # Heartbeat Request
+            # TODO, see if you can reset sleep timer on HeartBeat request (as this function will run immediately, resulting in an early follow-up heartbeat)
             case 1:
-                pass
+                msgObj = GatewayMessage(1, self._lastSequence)
+                await self.send(msgObj)
 
             # Reconnect
             case 7:
@@ -24,13 +44,43 @@ class GatewaySession(Gateway):
 
             # Hello
             case 10:
+                # Update heartbeat interval
+                if("heartbeat_interval" in msgObj.d):
+                    self.setHeartbeatInterval(msgObj.d["heartbeat_interval"])
+                # Identify to API
                 data = {"token": self.token, "properties": {"os": "Linux", "browser": "redTelephone", "device": "redTelephone"}, "intents": 1 << 9}
                 identifyMsg = GatewayMessage(2, data)
-                pass
+                await self.send(identifyMsg)
 
             # Heartbeat Ack
             case 11:
                 pass
 
             case _:
-                raise ValueError("Unsupported OP code in response {}".format(msgObj.op))
+                raise ValueError("Unsupported OP code in msg {}".format(msgObj.op))
+
+if __name__ == "__main__":
+
+    # Retrieve the discord bot token
+    try:
+        tokenFile = open("token.txt", 'r')
+        token = tokenFile.readline()
+    except:
+        print("ERROR: Unable to open/read token.txt")
+        sys.exit(1)
+
+    gw = Gateway(token)
+
+    @gw.eventHandler
+    def ready(msgObj):
+        print("xxxxxxxxxxxxxxxx")
+        print(msgObj)
+        print("XXXXXXXXXXXXXXXXX")
+
+    # asyncio.run(gw._run())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(gw._run())
+    finally:
+        loop.close()
--- gateway_connection.py
+++ gateway_connection.py
@@ -3,22 +3,25 @@ import websockets
 
 # Standard library
 import asyncio
+from typing import Any
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from types import SimpleNamespace
 
-URI = "wss://gateway.discord.gg"
+DEFAULT_URL = "wss://gateway.discord.gg/"
 API_VERSION = 10
 
 @dataclass
 class GatewayMessage:
     op: int
-    d: dict = None
+    d: Any = None
     s: int = None
-    t: int = None
+    t: str = None
 
     def stringify(self):
-        pass
+        classDict = asdict(self)
+        jsonString = json.dumps(classDict)
+        return jsonString
 
     @staticmethod
     def objectify(string):
@@ -27,27 +30,30 @@ class GatewayMessage:
         # Convert data field to a Simple Namespace
         # print(classObj.d)
         # classObj.d = json.loads(classObj.d, object_hook=lambda data: SimpleNamespace(**data))
-        # return classObj
+        return classObj
 
-class Gateway:
+class GatewayConnection:
+    url: str
+    params: str
     token: str
     _heartbeatInterval: float
     _lastSequence: int = None
     _sendQueue: asyncio.Queue
 
-    def __init__(self, token):
+    def __init__(self, token, url=DEFAULT_URL):
         self.token = token
+        self._url = url
+        self._params = "?v={}&encoding=json".format(API_VERSION)
         self._heartbeatInterval = 1
         self._lastSequence = None
         self._sendQueue = asyncio.Queue()
 
     async def _run(self):
-        webSocketURI = "{}/?v={}&encoding=json".format(URI, API_VERSION)
-        async with websockets.connect(webSocketURI, open_timeout=15) as websocket:
-            self._recvTask = asyncio.create_task(self._msgRecv(websocket))
-            self._sendTask = asyncio.create_task(self._msgSend(websocket))
-            self._heartbeatTask = asyncio.create_task(self._heartbeat())
-            # asyncio.gather(self._msgRecv(websocket), self._msgSend(websocket), self._heartbeat())
+        async with websockets.connect(self._url + self._params, open_timeout=15) as websocket:
+            self._recvTask = asyncio.create_task(self._recvLoop(websocket))
+            self._sendTask = asyncio.create_task(self._sendLoop(websocket))
+            self._heartbeatTask = asyncio.create_task(self._heartbeatLoop())
+            # asyncio.gather(self._recvLoop(websocket), self._sendLoop(websocket), self._heartbeatLoop())
             await self._recvTask
             await self._heartbeatTask
             await self._sendTask
@@ -57,49 +63,61 @@ class Gateway:
         self._sendTask.cancel()
         self._heartbeatTask.cancel()
 
-    async def _heartbeat(self):
+    async def _heartbeatLoop(self):
         while True:
-            lastSequence = self._lastSequence if self._lastSequence else "null"
-            msg = {"op": 1, "d": lastSequence}
-            await self._sendQueue.put(json.dumps(msg))
+            msgObj = GatewayMessage(1, self._lastSequence)
+            await self.send(msgObj)
             await asyncio.sleep(self._heartbeatInterval)
 
-    async def _msgSend(self, websocket):
+    async def _sendLoop(self, websocket):
         while True:
             msg = await self._sendQueue.get()
             print(msg)
             try:
                 await websocket.send(msg)
+
             except websockets.exceptions.ConnectionClosed as e:
                 print(e)
-                await self._stop()
+                await self._stop()       
             except Exception as e:
                 print(e)
 
-    async def _msgRecv(self, websocket):
+
+    async def _recvLoop(self, websocket):
             try:
                 async for msg in websocket:
                     print(msg)
                     msgObj = GatewayMessage.objectify(msg)
                     await self.processMsg(msgObj)
+
             except websockets.exceptions.ConnectionClosed as e:
                 print(e)
-                await self._stop()
+                await self._stop()       
+            except Exception as e:
+                print(e)
 
-    async def processMsg(msgObj):
+    async def send(self, msgObj):
+        await self._sendQueue.put(msgObj.stringify())
+
+    async def processMsg(self, msgObj):
         raise NotImplementedError
+    
+    def setURL(self, url):
+        self._url = url
 
     def setHeartbeatInterval(self, ms):
         self._heartbeatInterval = ms / 1000
 
-    # def setLastSequence(self, sequence):
-    #     self._lastSequence = sequence
+    def setLastSequence(self, sequence):
+        self._lastSequence = sequence
 
-g = Gateway()
-# asyncio.run(g._run())
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-try:
-    loop.run_until_complete(g._run())
-finally:
-    loop.close()
+
+if __name__ == "__main__":
+    g = GatewayConnection("alksdjfdsklf")
+    # asyncio.run(g._run())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(g._run())
+    finally:
+        loop.close()
