1f7f29ac8f6453c62e119d3539ea59d074783a2e
Dys13xic <campbell.leckie@hotmail.com>
2025-02-05 15:32:07 -0500

    Implement endCall method, add event handler support on call received, add asyncio event for active VOIP session, Implement setup of RTP/RTCP sockets when call accepted.

--- voip.py
+++ voip.py
@@ -18,14 +18,19 @@ class Voip():
             self.rtcpPort = rtpPort + 1
 
         self.sipEndpoint = None
-        self.activeDialog = None
         self.rtpEndpoint = None
         self.rtcpEndpoint = None
+        self.activeDialog = None
+        self._eventListeners = {}
+        self.sessionStarted = asyncio.Event()
+
+    def eventHandler(self, func):
+        self._eventListeners[func.__name__] = func
     
     async def run(self):
         loop = asyncio.get_event_loop()
         _, self.sipEndpoint = await loop.create_datagram_endpoint(
-        lambda: Sip(self.sipPort),
+        lambda: Sip(port=self.sipPort, callback=self.callReceived),
         local_addr=("0.0.0.0", self.sipPort),
         )
 
@@ -49,23 +54,63 @@ class Voip():
                 local_addr=('0.0.0.0', self.rtcpPort),
                 remote_addr=(remoteIP, remoteRtcpPort)
             )
-
+        # TODO why are these outside the if statement, and what happens if the call times out?
         self.rtpEndpoint = endpoint
         self.rtcpEndpoint = ctrlEndpoint
         self.activeDialog = dialog
+        self.sessionStarted.set()
 
-    async def end(self, session):
-        # await self.sipEndpoint.bye()
-        pass
+    async def endCall(self):
+        await self.sipEndpoint.bye(self.activeDialog)
 
-    async def callReceived(self):
-        pass
+        self.rtpEndpoint.stop()
+        self.rtcpEndpoint.stop()
+
+        self.activeDialog = None
+        self.rtpEndpoint, self.rtcpEndpoint = None, None
+
+        self.sessionStarted.clear()
+
+
+    async def callReceived(self, dialog):
+        endpoint = None
+
+        if dialog:
+            remoteIP = dialog.getRemoteIP()
+            remoteRtpPort, remoteRtcpPort = dialog.getRtpPorts()
+            ssrc = Voip.genSSRC()
+
+            loop = asyncio.get_event_loop()
+            _, endpoint = await loop.create_datagram_endpoint(
+            lambda: RtpEndpoint(ssrc, encrypted=False),
+            local_addr=("0.0.0.0", self.rtpPort),
+            remote_addr=(remoteIP, remoteRtpPort)
+            )
+
+            _, ctrlEndpoint = await loop.create_datagram_endpoint(
+                lambda: RtpEndpoint(ssrc, encrypted=False),
+                local_addr=('0.0.0.0', self.rtcpPort),
+                remote_addr=(remoteIP, remoteRtcpPort)
+            )
+
+        # TODO why are these outside the if statement, and what happens if the call times out?
+        self.rtpEndpoint = endpoint
+        self.rtcpEndpoint = ctrlEndpoint
+        self.activeDialog = dialog
+        self.sessionStarted.set()
+
+        # Call relevant event handler
+        if('call_received' in self._eventListeners.keys()):
+            await self._eventListeners['call_received']()
 
     def getRTPEndpoint(self):
         return self.rtpEndpoint
     
     def getRTCPEndpoint(self):
         return self.rtcpEndpoint
+    
+    def getSessionStarted(self):
+        return self.sessionStarted
 
     @staticmethod
     def genSSRC():
