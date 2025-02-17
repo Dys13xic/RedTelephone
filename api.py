14c3cbf2505f76983d31b74f063d2328dafa1c1c
Dys13xic <campbell.leckie@hotmail.com>
2025-02-16 19:11:52 -0500

    Implement basic REST API for creating text channel messages

---
+++ api.py
@@ -0,0 +1,18 @@
+import requests
+
+API_URL = 'https://discord.com/api'
+API_VERSION = 10
+
+# TODO replace with self-written async class
+class Api():
+    _token: str
+
+    def __init__(self, token):
+        self._token = token
+        self.endpoint = '{}/v{}'.format(API_URL, str(API_VERSION))
+        self.session = requests.Session()
+        self.session.headers = {'Authorization': 'Bot {}'.format(self._token)}
+
+    def simple_message_create(self, text, channelID):
+        data = {'content': text}
+        r = self.session.post(self.endpoint + '/channels/{}/messages'.format(channelID), data, headers=self.getHeaders())
