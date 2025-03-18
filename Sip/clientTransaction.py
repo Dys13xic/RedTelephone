cc71209a9bf2e734e6ee5a25503203c8fe4890bd
Dys13xic <campbell.leckie@hotmail.com>
2025-03-18 00:25:26 -0400

    Utilize new SipMessage/Request/Response in buildRequest, and notify the Transaction User of any Invite/Non-Invite transaction responses.

--- Sip/clientTransaction.py
+++ Sip/clientTransaction.py
@@ -1,4 +1,4 @@
-from .sipMessage import SipMessage
+from .sipMessage import SipMessage, SipRequest, SipResponse
 from .transaction import Transaction
 from .dialog import Dialog
 
@@ -20,7 +20,7 @@ class ClientTransaction(Transaction):
             if self.requestMethod == "ACK":
                 self.sequence = dialog.localSeq
             else:
-                self.sequence = dialog.locaSeq + 1
+                self.sequence = dialog.localSeq + 1
         
         else:
             self.fromTag = self._genTag()
@@ -33,13 +33,24 @@ class ClientTransaction(Transaction):
         Transaction._transactions[self.id] = self
 
     def buildRequest(self, method):
-        messageBody = ""
+        targetAddress = (self.remoteIP, self.remotePort)
+        viaAddress = (self.localIP, self.localPort)
+        viaParams = {'branch': self.branch}
+        fromURI = f'<sip:IPCall@{self.localIP}:{self.localPort}>'
+        fromParams = {'tag': self.fromTag}
+        toURI = f'<sip:{self.remoteIP}:{self.remotePort}>'
+        if self.toTag:
+            toParams = {'tag': self.toTag}
+        else:
+            toParams = {}
+        additionalHeaders = {'Contact': fromURI, 'Max-Forwards': 70}
+        if method == 'INVITE':
+            body = SipMessage._buildSDP(self.localIP, 5004)
+            additionalHeaders['Content-Type'] = 'application/sdp'
+        else:
+            body = ''
 
-        if method == "INVITE":
-            # TODO add parameter for specifiying RTP port
-            messageBody = SipMessage._buildSDP(self.localIP, 5004)
-
-        return SipMessage._buildMessage(method, (self.localIP, self.localPort), (self.remoteIP, self.remotePort), self.branch, self.callID, self.sequence, method, self.fromTag, self.toTag, messageBody)
+        return SipRequest(method, targetAddress, viaAddress, viaParams, fromURI, fromParams, toURI, toParams, self.callID, self.sequence, body, additionalHeaders)
 
     async def invite(self):
         self.state = "Calling"
@@ -63,22 +74,25 @@ class ClientTransaction(Transaction):
         # TODO handle possible transport error during request
         if response:
 
-            # Await non-Provisional response
-            while 100 <= response.statusCode <= 199:
-                response = await self.recvQueue.get()
+            if response.statusCode.isProvisional():
+                self.state = "Proceeding"
+                # Await non-Provisional response
+                while response.statusCode.isProvisional():
+                    await self.notifyTU(response)
+                    response = await self.recvQueue.get()
 
-            # Successfully opened dialog
-            if 200 <= response.statusCode <= 299:
+            if response.statusCode.isSuccessful():
+                await self.notifyTU(response)
                 rtpPort, rtcpPort = SipMessage._parseSDP(response.body)
-                self.dialog = Dialog(self.callID, self.fromTag, "sip:IPCall@{}:{}".format(self.localIP, self.localPort), self.sequence, response.toTag, "sip:{}:{}".format(self.remoteIP, self.remotePort), response.additionalHeaders['Contact'].strip('<>'), rtpPort=rtpPort, rtcpPort=rtcpPort)
+                self.dialog = Dialog(self.callID, self.fromTag, "sip:IPCall@{}:{}".format(self.localIP, self.localPort), self.sequence, response.toParams['tag'], "sip:{}:{}".format(self.remoteIP, self.remotePort), response.additionalHeaders['Contact'].strip('<>'), rtpPort=rtpPort, rtcpPort=rtcpPort)
 
                 # Ack in seperate transaction
-                newTransaction = ClientTransaction(self.sendToTransport, "ACK", (self.localIP, self.localPort), (self.remoteIP, self.remotePort), self.dialog)
+                newTransaction = ClientTransaction(self.notifyTU, self.sendToTransport, "ACK", (self.localIP, self.localPort), (self.remoteIP, self.remotePort), self.dialog)
                 newTransaction.ack(autoClean=True)
 
-            # Failed to open dialog
-            elif 300 <= response.statusCode <= 699:
+            elif response.statusCode.isUnsuccessful():
                 self.state = "Completed"
+                await self.notifyTU(response)
                 self.ack()
 
                 # Answer duplicate final responses for 32 seconds before terminating transaction
@@ -86,7 +100,7 @@ class ClientTransaction(Transaction):
                     async with asyncio.timeout(Transaction.ANSWER_DUPLICATES_DURATION):
                         while(True):
                             response = await self.recvQueue.get()
-                            if 300 <= response.statusCode <= 699:
+                            if response.statusCode.isUnsuccessful():
                                 self.ack()
                             else:
                                 # TODO Maybe return a malformed response? *at the very least shouldn't cause program exit
@@ -117,7 +131,7 @@ class ClientTransaction(Transaction):
 
         async with asyncio.timeout(transactionTimeout):
             attempts = 0
-            while(not response or 100 <= response.statusCode <= 199):
+            while(not response or response.statusCode.isProvisional()):
                 self.sendToTransport(request, (self.remoteIP, self.remotePort))
 
                 retransmitInterval = (pow(2, attempts) * Transaction.T1)
@@ -126,17 +140,17 @@ class ClientTransaction(Transaction):
                 try:
                     async with asyncio.timeout(retransmitInterval):
                         response = await self.recvQueue.get()
-                        # TODO does this state update provide any value?
-                        if 100 <= response.statusCode <= 199:
+                        if response.statusCode.isProvisional():
                             self.state = 'Proceeding'
+                            await self.notifyTU(response)
                 except TimeoutError:
                     attempts += 1
 
         # TODO handle possible transport error during request
-
         if response:
-            if 300 <= response.statusCode <= 699:
+            if response.statusCode.isFinal():
                 self.state = 'Completed'
+                await self.notifyTU(response)
                 # Buffer response retransmissions
                 try:
                     async with asyncio.timeout(Transaction.T4):
@@ -145,14 +159,15 @@ class ClientTransaction(Transaction):
                 except TimeoutError:
                     pass
         
-        self.terminate()
         if method == 'BYE':
             self.dialog.terminate()
 
+        self.terminate()
+
     # TODO why is autoclean necessary again? Is there a better way to handle this?
     def ack(self, autoClean=False):
-        outgoingMsg = self.buildRequest("ACK")
-        self.sendToTransport(outgoingMsg, (self.remoteIP, self.remotePort))
+        request = self.buildRequest("ACK")
+        self.sendToTransport(request, (self.remoteIP, self.remotePort))
 
         if autoClean:
             self.terminate()
