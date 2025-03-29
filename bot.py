ed984546f69e09d9258e77b07088615c37c68411
Dys13xic <campbell.leckie@hotmail.com>
2025-03-29 02:17:35 -0400

    Implement config file loading.

--- bot.py
+++ bot.py
@@ -4,31 +4,18 @@ from rtp import RtpEndpoint
 from voip import Voip
 from doNotDisturb import DoNotDisturb, Weekdays
 from callLog import CallLog
+from config import Config
 
 # Standard Library
 import sys
 import asyncio
 import logging
 import os
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta, timezone
 
 LOGGING = True
 
-VOIP_HANDSET_ADDRESS = '10.13.0.6'
-RTP_PORT = 5004
-RTCP_PORT = 5005
-SIP_PORT = 5060
-
-HOME_GUILD_ID = '729825988443111424'
-HOME_VOICE_CHANNEL_ID = '729825988443111428'
-HOME_TEXT_CHANNEL_ID = '733403603867271179'
-
-HOURLY_CALL_LIMIT = 5
-
-UTC_OFFSET_HOURS = 5
-UTC_OFFSET_FACTOR = -1
-
-if __name__ == "__main__":
+async def main():
     # Retrieve the discord bot token
     try:
         with open('token.txt', 'r', encoding='utf-8') as f:
@@ -36,20 +23,20 @@ if __name__ == "__main__":
     except Exception:
         sys.exit()
 
-    # TODO load configuration settings
+    # Load configuration settings
+    config = Config()
+    await config.load()
 
-    currentTimeZone = timezone(UTC_OFFSET_FACTOR * timedelta(hours=UTC_OFFSET_HOURS))
-    doNotDisturb = DoNotDisturb(timeFrames=[(0, 9), (23, 24)], tz=currentTimeZone)
-    callLog = CallLog(HOURLY_CALL_LIMIT, tz=currentTimeZone)
+    currentTimeZone = timezone(config.utcOffsetFactor * timedelta(hours=config.utcOffset))
+    doNotDisturb = DoNotDisturb(config.doNotDisturbTimes, tz=currentTimeZone)
+    callLog = CallLog(config.hourlyCallLimit, tz=currentTimeZone)
 
     client = Client(token)
-    voip = Voip(SIP_PORT, RTP_PORT, RTCP_PORT)
-
+    voip = Voip(config.publicIP)
 
     @client.event
     async def on_user_mention(msgData):
         authorID = msgData['author']['id']
-        # TODO is it more appropriate to keep the stuff users would see to client?
         voiceServerID, voiceChannelID = client.gateway.getVoiceState(authorID)
         _, botVoiceChannelID = client.gateway.getVoiceState(client.gateway.userID)
         if msgData['guild_id'] == voiceServerID and voiceChannelID:
@@ -64,7 +51,7 @@ if __name__ == "__main__":
                 client.createMessage('`The line is already in use.`', msgData['channel_id'])
                 pass
             else:
-                await asyncio.gather(client.joinVoice(voiceServerID, voiceChannelID), voip.call(VOIP_HANDSET_ADDRESS))
+                await asyncio.gather(client.joinVoice(voiceServerID, voiceChannelID), voip.call(config.voipAddress))
                 callLog.record()
         else:
             client.createMessage('`User must be in a voice channel to initiate a call.`', msgData['channel_id'])
@@ -80,8 +67,8 @@ if __name__ == "__main__":
 
     @voip.event
     async def on_inbound_call():
-        await client.joinVoice(HOME_GUILD_ID, HOME_VOICE_CHANNEL_ID)
-        client.createMessage('@everyone', HOME_TEXT_CHANNEL_ID)
+        await client.joinVoice(config.discordGuildID, config.discordVoiceChannelID)
+        client.createMessage(config.incomingCallMessage, config.discordTextChannelID)
 
     @voip.event
     async def on_inbound_call_ended():
@@ -92,16 +79,16 @@ if __name__ == "__main__":
     async def on_inbound_call_cancelled():
         await client.leaveVoice()
 
-    async def main():
-        if LOGGING:
-            logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-            os.environ['WEBSOCKETS_MAX_LOG_SIZE'] = '1000'
-        await asyncio.gather(client.run(), voip.run())
+    if LOGGING:
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+        os.environ['WEBSOCKETS_MAX_LOG_SIZE'] = '1000'
+    await asyncio.gather(client.run(), voip.run())
 
+
+if __name__ == "__main__":
     try:
         asyncio.run(main(), debug=LOGGING)
     except KeyboardInterrupt:
         print('Process interrupted')
     finally:
         print('Bot successfully shutdown')
-    
---
+++ config.py
@@ -0,0 +1,70 @@
+# Standard Library
+from configparser import ConfigParser
+from aiohttp import ClientSession
+import json
+
+DEFAULT_CONFIG_FILE = 'config.ini'
+REQUIRED_FIELDS = {
+    'Server': ['PublicIP'],
+    'VoIP': ['Address'],
+    'Discord': ['HomeGuildID', 'HomeVoiceChannelID', 'HomeTextChannelID'],
+    'Messages': ['Welcome', 'IncomingCall'],
+    'Timezone': ['UtcOffset']
+}
+IP_DISCOVERY_ENDPOINT = 'https://checkip.amazonaws.com/'
+
+
+class Config():
+    def __init__(self):
+        self.publicIP = None
+        self.voipAddress = None
+        self.voipWhiteList = []
+        self.discordGuildID = None
+        self.discordVoiceChannelID = None
+        self.discordTextChannelID = None
+        self.welcomeMessage = None
+        self.incomingCallMessage = None
+        self.utcOffset = None
+        self.utcOffsetFactor = None
+        self.hourlyCallLimit = None
+        self.doNotDisturbTimes = []
+
+    async def load(self, filename=DEFAULT_CONFIG_FILE):
+        config = ConfigParser()
+        config.read(filename)
+
+        for section, options in REQUIRED_FIELDS.items():
+            for o in options:
+                if not config.has_option(section, o):
+                    raise Exception(f'Mandatory parameter "{o}" missing from [{section}] section in config.ini')
+
+        self.publicIP = config.get('Server', 'PublicIP')
+        if self.publicIP == 'auto':
+            self.publicIP = await self._getPublicIP()
+
+        self.voipAddress = config.get('VoIP', 'Address')
+        
+        temp = config.get('VoIP', 'Whitelist', fallback='')
+        self.voipWhiteList = temp.split(',')
+
+        self.discordGuildID = config.get('Discord', 'HomeGuildID')
+        self.discordVoiceChannelID = config.get('Discord', 'HomeVoiceChannelID')
+        self.discordTextChannelID = config.get('Discord', 'HomeTextChannelID')
+        self.welcomeMessage = config.get('Messages', 'Welcome')
+        self.incomingCallMessage = config.get('Messages', 'IncomingCall')
+
+        temp = config.getint('Timezone', 'UtcOffset')
+        self.utcOffsetFactor = 1 if temp >= 0 else -1
+        self.utcOffset = abs(temp)
+
+        self.hourlyCallLimit = config.getint('Call Preferences', 'HourlyCallLimit', fallback=0)
+        if not self.hourlyCallLimit:
+            self.hourlyCallLimit = None
+
+        self.doNotDisturb = json.loads(config.get('Call Preferences', 'DoNotDisturb', fallback='[]'))
+
+    async def _getPublicIP(self):
+        async with ClientSession() as session:
+            async with session.get(IP_DISCOVERY_ENDPOINT) as resp:
+                ip = await resp.text()
+                return ip.strip()
