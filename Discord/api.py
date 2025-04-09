b4031ead31e487f3861f4681550c9f92ec8f2435
Dys13xic <campbell.leckie@hotmail.com>
2025-04-08 21:07:46 -0400

    Add get_user_voice_state API call.

--- Discord/api.py
+++ Discord/api.py
@@ -15,13 +15,18 @@ class Api():
         self.endpoint = f'{API_URL}/v{str(API_VERSION)}/'
         # TODO establish proper versioning constants
         self.headers = {'User-Agent': 'DiscordBot (RedTelephone, 1.0)', 'Authorization': f'Bot {self._token}'}
-        self.session = None
+        self.session = ClientSession(base_url = self.endpoint, headers=self.headers)
 
     # TODO add error handling
     async def simple_message_create(self, text, channelID):
-        if not self.session:
-            self.session = ClientSession(base_url = self.endpoint, headers=self.headers)
-
         resp = await self.session.post(f'channels/{channelID}/messages', data={'content': text})
+
+    async def get_user_voice_state(self, userID, guildID):
+        async with self.session.get(f'guilds/{guildID}/voice-states/{userID}') as resp:
+            result = await resp.json()
+            guildID = result.get('guild_id', None)
+            channelID = result.get('channel_id', None)
+
+        return guildID, channelID
 
     # TODO add cleanup method to close session
