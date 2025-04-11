5da1249ae9f81e6b828ee473cb251c856dc7cee0
Dys13xic <campbell.leckie@hotmail.com>
2025-04-11 01:36:55 -0400

    Add Client.cleanup() method to gracefully shutdown API connections, call cleanup() in finally block of run(). Rename user_mention event to bot_mention and voice_secret_received to voice_connection_finalized. Handle voice_server_update with NULL endpoint, move handling of updateVoiceChannel out of VoiceGateway.disconnect() and into client.leaveVoice(). Make guildID an optional parameter in fetchVoiceState() and attempt to retrieve it from voice state. Add docstrings and comments.

--- Discord/client.py
+++ Discord/client.py
@@ -8,6 +8,7 @@ from .api import Api
 import asyncio
 
 class Client:
+    """Manage user facing interaction with Discord's Gateway, VoiceGateway, and REST API"""
     _token: str
     gatewayEventHandler: EventHandler
     voiceEventHandler: EventHandler
@@ -32,13 +33,16 @@ class Client:
         self.voiceEventHandler.on('session_description', self.on_session_description)
 
     async def run(self):
+        """Start the bot and connect to Discord."""
         try:
             await self.gateway.connect()
         except asyncio.CancelledError:
             print('Gateway cancelled.')
+        finally:
+            await self.cleanup()
 
-    # Register client events through function decorator
     def event(self, func):
+        """Register client events through function decorator."""
         async def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             if asyncio.iscoroutine(result):
@@ -47,39 +51,55 @@ class Client:
         
         self.eventHandler.on(func.__name__.removeprefix('on_'), wrapper)
         return wrapper
+    
+    async def cleanup(self):
+        """Cleanup client connections."""
+        if self.voiceGateway:
+            await self.voiceGateway.disconnect()
+
+        await self.gateway.disconnect()
+        await self.api.close()
 
     # Gateway Events
     # ---------------
     async def on_guild_create(self, data):
-        # TODO keep a file of 
+        """When a bot connects/reconnects to a guild, determine if the guild is new (if so dispatch an event)."""
+        # TODO implement logic for determining if the guild is new
         await self.eventHandler.dispatch('guild_join', data)
 
     async def on_message_create(self, data):
+        """When a message is received, check if the bot was mentioned (if so dispatch an event)."""
         userID = self.gateway.userID
         for user in data['mentions']:
             if user['id'] == userID:
-                await self.eventHandler.dispatch('user_mention', data)
+                await self.eventHandler.dispatch('bot_mention', data)
                 break
 
     async def on_voice_server_update(self, token, endpoint):
-        # TODO handle when endpoint == NULL
-        self.voiceGateway.token = token
-        self.voiceGateway.endpoint = endpoint
-        asyncio.create_task(self.voiceGateway.connect())
+        """When a VOICE_SERVER_UPDATE is received, configure and connect the voice gateway."""
+        if endpoint:
+            self.voiceGateway.token = token
+            self.voiceGateway.endpoint = endpoint
+            asyncio.create_task(self.voiceGateway.connect())
+        else:
+            await self.gateway.updateVoiceChannel(channelID=None)
 
     # Voice Gateway Events
     # ---------------------
     async def on_session_description(self):
-        await self.eventHandler.dispatch('voice_secret_received')
-
+        """When a SESSION_DESCRIPTION is received, dispatch an event indicating voice connection is complete."""
+        await self.eventHandler.dispatch('voice_connection_finalized')
 
     # Gateway API Methods
     # --------------------
     async def joinVoice(self, guildID, channelID):
+        """Join a new voice channel."""
         await self.gateway.updateVoiceChannel(guildID, channelID)
         self.voiceGateway = VoiceGateway(self.gateway, guildID, channelID, self.voiceEventHandler.dispatch)
 
     async def leaveVoice(self):
+        """Leave the current voice channel."""
+        await self.gateway.updateVoiceChannel(channelID=None)
         if self.voiceGateway:
             await self.voiceGateway.disconnect()
             self.voiceGateway = None
@@ -87,17 +107,20 @@ class Client:
     # REST API Wrapper Methods
     # -----------------
     def createMessage(self, text, channelID):
+        """Send a message to the specified text channel."""
         asyncio.create_task(self.api.simple_message_create(text, channelID))
 
     # Fetch Methods
     # -----------------
-    async def fetchVoiceState(self, userID, guildID):
+    async def fetchVoiceState(self, userID, guildID=None):
+        """Return the current voice state of a user using cached values if they exist or a REST API call."""
         cachedGuildID, cachedVoiceID = self.gateway.getVoiceState(userID)
         if cachedGuildID and cachedVoiceID:
             guildID, voiceID = cachedGuildID, cachedVoiceID
-
-        else:
+        elif guildID:
             guildID, voiceID = await self.api.get_user_voice_state(userID, guildID)
             self.gateway.setVoiceState(userID, (guildID, voiceID))
+        else:
+            guildID, voiceID = None, None
                 
         return guildID, voiceID
