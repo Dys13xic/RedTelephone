f8390c297704f950d2e11f9a80e3d4d02a15deb4
Dys13xic <campbell.leckie@hotmail.com>
2025-04-11 01:01:59 -0400

    Remove on_inbound_call_cancelled listener, rename on_voice_secret_received listener to on_voice_connection_finalized, replace getVoiceState call with fetchVoiceState. Add docstrings and comments.

--- bot.py
+++ bot.py
@@ -17,26 +17,30 @@ from datetime import timedelta, timezone
 LOGGING = True
 
 async def main():
-    # Load configuration settings
+    # Load config.ini settings
     config = Config()
     await config.load()
 
-    currentTimeZone = timezone(config.utcOffsetFactor * timedelta(hours=config.utcOffset))
-    doNotDisturb = DoNotDisturb(config.doNotDisturbTimes, tz=currentTimeZone)
-    callLog = CallLog(config.hourlyCallLimit, tz=currentTimeZone)
-
+    # Initialize main services
     client = Client(token=config.discordBotToken)
     voip = Voip(config.publicIP, allowList=[config.voipAddress] + config.voipAllowList)
 
+    # Initialize utilities
+    currentTimeZone = timezone(config.utcOffsetFactor * timedelta(hours=config.utcOffset))
+    doNotDisturb = DoNotDisturb(config.doNotDisturbTimes, tz=currentTimeZone)
+    callLog = CallLog(config.hourlyCallLimit, tz=currentTimeZone)
+    
+    # Event listeners
     @client.event
     async def on_guild_join(msgData):
+        """Posts a welcome message in a text channel when a new guild is joined."""
         pass
 
     @client.event
-    async def on_user_mention(msgData):
+    async def on_bot_mention(msgData):
+        """When the bot is mentioned in a text channel, join the message author's current voice channel and call the VoIP handset."""
         voiceServerID, voiceChannelID = await client.fetchVoiceState(msgData['author']['id'], msgData['guild_id'])
-        # TODO replace use of getVoiceState somehow
-        _, botVoiceChannelID = client.gateway.getVoiceState(client.gateway.userID)
+        _, botVoiceChannelID = await client.fetchVoiceState(client.gateway.userID)
 
         if voiceServerID and voiceChannelID:
             if doNotDisturb.violated():
@@ -47,7 +51,7 @@ async def main():
 
             elif botVoiceChannelID:
                 client.createMessage('`The line is already in use.`', msgData['channel_id'])
-                pass
+
             else:
                 try:
                     result = await asyncio.gather(client.joinVoice(voiceServerID, voiceChannelID), voip.call(config.voipAddress))
@@ -59,35 +63,37 @@ async def main():
             client.createMessage('`User must be in a voice channel to initiate a call.`', msgData['channel_id'])
 
     @client.event
-    async def on_voice_secret_received():
+    async def on_voice_connection_finalized():
+        """Once voice communication to Discord is finalized, answer if the call is incoming and  """
         # TODO improve the appearance of this code and only run when needed (incoming calls)
         voip.answerCall.set()
 
-        # Need an active VOIP session before proxying traffic
+        # Wait for an active VoIP session before proxying traffic
         await voip.sessionStarted.wait()
+        # Notify voice gateway that audio packets are starting to be sent
         await client.voiceGateway.updateSpeaking()
         RtpEndpoint.proxy(client.voiceGateway.rtpEndpoint, voip.rtpEndpoint, yCtrl=voip.rtcpEndpoint)
 
     @voip.event
     async def on_inbound_call():
+        """On an incoming call, join the configured discord voice channel and notify guild members with a message."""
         await client.joinVoice(config.discordGuildID, config.discordVoiceChannelID)
         client.createMessage(config.incomingCallMessage, config.discordTextChannelID)
 
     @voip.event
     async def on_inbound_call_ended():
-        await client.leaveVoice()
-        # TODO cleanup the RTP assets?
-
-    @voip.event
-    async def on_inbound_call_cancelled():
+        """When a call is remotely terminated, leave the discord voice channel."""
         await client.leaveVoice()
 
+    # Configure logging
     if LOGGING:
         logging.basicConfig(format='%(message)s', level=logging.DEBUG)
         os.environ['WEBSOCKETS_MAX_LOG_SIZE'] = '1000'
+
+    # Run main co-routines
     await asyncio.gather(client.run(), voip.run())
 
-
+# Entry point
 if __name__ == "__main__":
     try:
         asyncio.run(main(), debug=LOGGING)
