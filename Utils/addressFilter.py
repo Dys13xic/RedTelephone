52c8046384f7228c9a0bc9c8566585286cddff3e
Dys13xic <campbell.leckie@hotmail.com>
2025-04-11 11:50:15 -0400

    Replace magic number with RESOLVE_INTERVAL constant, add underscore to _updateHostname() indicating private helper method. Add doc strings and comments.

--- Utils/addressFilter.py
+++ Utils/addressFilter.py
@@ -2,7 +2,10 @@ import asyncio
 import socket
 import concurrent.futures
 
+RESOLVE_INTERVAL = 300
+
 class AddressFilter():
+    """Maintain a list of allowed IPs and domains."""
     def __init__(self, addresses):
         self._addressSet = set()
         self._addressMap = {}
@@ -15,14 +18,17 @@ class AddressFilter():
                 self._addressMap[address] = None
 
     async def run(self):
+        """Retrieve domain IP addresses every RESOLVE_INTERVAL seconds."""
         while self._addressMap:
             await self.resolveDomains() 
-            await asyncio.sleep(300)
+            await asyncio.sleep(RESOLVE_INTERVAL)
 
     async def resolveDomains(self):
+        """Query DNS for the current IP of each domain in _addressMap and update their value."""
+        # Run queries in a thread pool to prevent blocking.
         loop = asyncio.get_running_loop()
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
-        futures = [loop.run_in_executor(executor, updateHostname, domainName) for domainName in self._addressMap.keys()]
+        futures = [loop.run_in_executor(executor, _updateHostname, domainName) for domainName in self._addressMap.keys()]
         results = await asyncio.gather(*futures)
 
         for domainName, address in results:
@@ -31,6 +37,7 @@ class AddressFilter():
         self.initialized.set()
 
     def getAddresses(self):
+        """Return a list of IPs in _addressSet and _addressMap."""
         result = self._addressSet.copy()
         for address in self._addressMap.values():
             if (address):
@@ -38,8 +45,10 @@ class AddressFilter():
 
         return result
     
-def updateHostname(domainName):
+def _updateHostname(domainName):
+    """Helper method to return the domainName and the result of its DNS query."""
     return domainName, socket.gethostbyname(domainName)
 
 def _isIP(address):
+    """Crude helper method to determine if the address is an IP."""
     return address.replace('.', '').isnumeric()
