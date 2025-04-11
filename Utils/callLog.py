1dddfdb897372240425f409d31557ef529c4b57d
Dys13xic <campbell.leckie@hotmail.com>
2025-04-11 11:59:11 -0400

    Add docstrings and comments.

--- Utils/callLog.py
+++ Utils/callLog.py
@@ -2,22 +2,27 @@ from collections import deque
 from datetime import datetime, timezone, timedelta
 
 class CallLog:
+    """Manage a log of the last 'hourlyLimit' calls."""
 
     def __init__(self, hourlyLimit, tz=timezone.utc):
         self.calls = deque(maxlen=hourlyLimit)
         self.tz = tz
 
     def record(self):
+        """Record a new call to the log."""
         currentDateTime = datetime.now(tz=self.tz)
         self.calls.append(currentDateTime)
 
     def callLimitExceeded(self):
+        """Determine if the hourly call limit was exceeded."""
         return bool(self.nextAllowedTime())
 
     def nextAllowedTime(self):
+        """If the call limit has been reached, Returns the next available time in hours:mins:seconds. Otherwise returns None."""
         currentDateTime = datetime.now(tz=self.tz)
 
         if len(self.calls) == self.calls.maxlen:
+            # Add an hour to the oldest entry in the call log.
             nextAllowedTime = self.calls[0] + timedelta(hours=1)
             if nextAllowedTime > currentDateTime:
                 return nextAllowedTime.strftime('%I:%M:%S')
