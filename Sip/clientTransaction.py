ac09da91b3d167ed0f935f79080bc39bf13e059f
Dys13xic <campbell.leckie@hotmail.com>
2025-04-14 01:37:56 -0400

    Replace state string with States(Enum). Increment dialog local sequence on transaction creation. Add optional overrideTransaction argument to constructor and refactor cancelFromInvite. Remove acking of successful responses from transaction. Add passing of exceptions to transaction user for invite() and non-invite method(). Add check to noninvited() to ensure a dialog exists for requests other than Cancel. Add docstrings and comments.

--- Sip/clientTransaction.py
+++ Sip/clientTransaction.py
@@ -1,178 +1,189 @@
-from .sipMessage import SipMessage, SipRequest, SipResponse
-from .transaction import Transaction
-from .dialog import Dialog
+# 1st Party
+from .sipMessage import SipMessage, SipRequest
+from .transaction import Transaction, States
 
+# Standard Library
 import asyncio
 import time
 import random
 import hashlib
 
 class ClientTransaction(Transaction):
-    receivedProvisional: asyncio.Event
-
-    def __init__(self, notifyTU, sendToTransport, requestMethod, localAddress, remoteAddress, dialog=None):
+    """Manage the state of a SIP request across many independent messages."""
+    def __init__(self, notifyTU, sendToTransport, requestMethod, localAddress, remoteAddress, dialog=None, overrideTransaction=None):
         super().__init__(notifyTU, sendToTransport, requestMethod, localAddress, remoteAddress, dialog)
         
         if(self.dialog):
-            self.fromTag = dialog.localTag
-            self.toTag = dialog.remoteTag
-            self.callID = dialog.callID
-
-            # TODO update dialog settings after request sent
-            if self.requestMethod == "ACK":
-                self.sequence = dialog.localSeq
-            else:
-                self.sequence = dialog.localSeq + 1
-        
+            # Get header values from existing dialog
+            self.fromTag = self.dialog.localTag
+            self.toTag = self.dialog.remoteTag
+            self.callID = self.dialog.callID
+            # Increment dialog sequence number
+            if self.requestMethod != "ACK":
+                self.dialog.localSeq += 1
+            self.sequence = self.dialog.localSeq
         else:
+            # Generate new header values
             self.fromTag = self._genTag()
             self.toTag = ""
             self.callID = self._genCallID()
             self.sequence = 1
-
+        
         self.branch = self._genBranch()
+        self.receivedProvisional: asyncio.Event = asyncio.Event()
+
+        # If included, override properties with those of an existing transaction (used for cancelling invites)
+        if overrideTransaction:
+            self.branch = overrideTransaction.branch
+            self.fromTag = overrideTransaction.fromTag                                                                                                              
+            self.toTag = overrideTransaction.toTag
+            self.callID = overrideTransaction.callID
+            self.sequence = overrideTransaction.sequence
+
+        # Register new transaction
         self.id = self.branch + self.requestMethod
-        self.receivedProvisional = asyncio.Event()
         Transaction._transactions[self.id] = self
 
     def cancelFromInvite(self):
+        """Construct a cancel transaction from an existing invite transaction."""
         if self.requestMethod != 'INVITE':
-            raise ValueError
+            raise ValueError('Non-Invite transaction.')
         
-        cancel = ClientTransaction(self.notifyTU, self.sendToTransport, 'CANCEL', (self.localIP, self.localPort), (self.remoteIP, self.remotePort), dialog=None)
-        del Transaction._transactions[cancel.id]
-
-        cancel.branch = self.branch
-        cancel.fromTag = self.fromTag
-        cancel.toTag = self.toTag
-        cancel.callID = self.callID
-        cancel.sequence = self.sequence
-        cancel.id = cancel.branch + cancel.requestMethod
-        cancel.receivedProvisional = asyncio.Event()
-        Transaction._transactions[cancel.id] = cancel
-        return cancel
+        return ClientTransaction(self.notifyTU, self.sendToTransport, 'CANCEL', (self.localIP, self.localPort), (self.remoteIP, self.remotePort), dialog=None, overrideTransaction=self)
 
     def buildRequest(self, method):
+        """Build a SIP request of the specified method."""
+        # Configure mandatory headers
         targetAddress = (self.remoteIP, self.remotePort)
         viaAddress = (self.localIP, self.localPort)
-        viaParams = {'branch': self.branch}
         fromURI = f'<sip:IPCall@{self.localIP}:{self.localPort}>'
-        fromParams = {'tag': self.fromTag}
         toURI = f'<sip:{self.remoteIP}:{self.remotePort}>'
+
+        # Configure header parameters
+        viaParams = {'branch': self.branch}
+        fromParams = {'tag': self.fromTag}
         if self.toTag:
             toParams = {'tag': self.toTag}
         else:
             toParams = {}
+
+        # Configure non-mandatory headers
         additionalHeaders = {'Contact': fromURI, 'Max-Forwards': 70}
+
+        # Configure message body
         if method == 'INVITE':
-            body = SipMessage._buildSDP(self.localIP, 5004)
             additionalHeaders['Content-Type'] = 'application/sdp'
+            # TODO replace magic numbers
+            body = SipMessage._buildSDP(self.localIP, 5004)
         else:
             body = ''
 
         return SipRequest(method, targetAddress, viaAddress, viaParams, fromURI, fromParams, toURI, toParams, self.callID, self.sequence, body, additionalHeaders)
 
     async def invite(self):
+        """Manage a SIP invite request."""
         self.dialog = None
-        self.state = "Calling"
+        self.state = States.CALLING
         request = self.buildRequest("INVITE")
-
         transactionTimeout = 64 * Transaction.T1
         response = None
 
-        async with asyncio.timeout(transactionTimeout):
-            attempts = 0
-            while(not response):
-                self.sendToTransport(request, (self.remoteIP, self.remotePort))
-                retransmitInterval = (pow(2, attempts) * Transaction.T1)
+        try:
+            # Send request with exponential back-off until a response is received or transaction timeout reached.
+            async with asyncio.timeout(transactionTimeout):
+                attempts = 0
+                while(not response):
+                    # Send/resend request
+                    self.sendToTransport(request, (self.remoteIP, self.remotePort))
+                    retransmitInterval = (pow(2, attempts) * Transaction.T1)
+                    try:
+                        # Wait (up to) the retransmit interval duration for a response before re-attempting
+                        async with asyncio.timeout(retransmitInterval):
+                            response = await self.recvQueue.get()
+                    except TimeoutError:
+                        attempts += 1
 
-                try:
-                    async with asyncio.timeout(retransmitInterval):
-                        response = await self.recvQueue.get()
-                except TimeoutError:
-                    attempts += 1
-
-        # TODO handle possible transport error during request
-        if response:
-
+            # Await a non-Provisional response
             if response.statusCode.isProvisional():
-                self.state = "Proceeding"
+                self.state = States.PROCEEDING
                 self.receivedProvisional.set()
-                # Await non-Provisional response
                 while response.statusCode.isProvisional():
                     await self.notifyTU(response)
                     response = await self.recvQueue.get()
 
             if response.statusCode.isSuccessful():
                 await self.notifyTU(response)
-                rtpPort, rtcpPort = SipMessage._parseSDP(response.body)
-                self.dialog = Dialog(self.callID, self.fromTag, f'sip:IPCall@{self.localIP}:{self.localPort}', self.sequence, response.toParams['tag'], f'sip:{self.remoteIP}:{self.remotePort}', response.additionalHeaders['Contact'].strip('<>'), rtpPort=rtpPort, rtcpPort=rtcpPort)
-
-                # Ack in seperate transaction
-                newTransaction = ClientTransaction(self.notifyTU, self.sendToTransport, "ACK", (self.localIP, self.localPort), (self.remoteIP, self.remotePort), self.dialog)
-                newTransaction.ack(autoClean=True)
                 self.terminate()
 
             elif response.statusCode.isUnsuccessful():
-                self.state = "Completed"
+                self.state = States.COMPLETED
                 await self.notifyTU(response)
                 self.ack()
-                # Answer duplicate final responses for 32 seconds before terminating transaction
+                # Acknowledge duplicate final responses for duration before terminating transaction
                 asyncio.create_task(self._handleRetransmissions(duration=Transaction.ANSWER_DUPLICATES_DURATION))
 
             else:
-                # Invalid response code TODO generate a malformed request response? *Note: could also do this at a lower level.
-                print("Invalid response code")
-                self.terminate()
-                exit()
+                raise ValueError('Invalid SIP response code')
+
+        except (ConnectionError, TimeoutError, ValueError) as e:
+            # Pass exceptions to the Transaction User and terminate the transaction
+            self.notifyTU(e)
+            self.terminate()
 
         return self.dialog
 
     async def nonInvite(self, method):
-        # TODO Ensure dialog established (Except for Cancel)
-        # if not self.dialog:
-        #     print("No dialog")
-        #     exit()
-        self.state = "Trying"
-        request = self.buildRequest(method)
+        """Manage a SIP non-invite request."""
+        if not self.dialog and method != 'Cancel':
+            raise ValueError('Missing dialog.')
 
+        self.state = States.TRYING
+        request = self.buildRequest(method)
         transactionTimeout = 64 * Transaction.T1
         response = None
 
-        async with asyncio.timeout(transactionTimeout):
-            attempts = 0
-            while(not response or response.statusCode.isProvisional()):
-                self.sendToTransport(request, (self.remoteIP, self.remotePort))
+        try:
+            # Send request with exponential back-off until a final response is received or transaction timeout reached.
+            async with asyncio.timeout(transactionTimeout):
+                attempts = 0
+                while(not response or response.statusCode.isProvisional()):
+                    # Send/resend request
+                    self.sendToTransport(request, (self.remoteIP, self.remotePort))
+                    # Cap retransmit interval at T2
+                    retransmitInterval = (pow(2, attempts) * Transaction.T1)
+                    retransmitInterval = min(Transaction.T2, retransmitInterval)
+                    try:
+                        # Wait (up to) the retransmit interval duration for a final response before re-attempting
+                        async with asyncio.timeout(retransmitInterval):
+                            response = await self.recvQueue.get()
+                            if response.statusCode.isProvisional():
+                                self.state = States.PROCEEDING
+                                await self.notifyTU(response)
+                    except TimeoutError:
+                        attempts += 1
 
-                retransmitInterval = (pow(2, attempts) * Transaction.T1)
-                retransmitInterval = min(Transaction.T2, retransmitInterval)
-
-                try:
-                    async with asyncio.timeout(retransmitInterval):
-                        response = await self.recvQueue.get()
-                        if response.statusCode.isProvisional():
-                            self.state = 'Proceeding'
-                            await self.notifyTU(response)
-                except TimeoutError:
-                    attempts += 1
-
-        # TODO handle possible transport error during request
-        if response:
             if response.statusCode.isFinal():
-                self.state = 'Completed'
+                self.state = States.COMPLETED
                 await self.notifyTU(response)
-                # Buffer response retransmissions
+                # Buffer response retransmissions for duration before terminating transaction
                 asyncio.create_task(self._handleRetransmissions(duration=Transaction.T4))
+            else:
+                raise ValueError('Invalid SIP response code')
 
-    # TODO why is autoclean necessary again? Is there a better way to handle this?
+        except (ConnectionError, TimeoutError, ValueError) as e:
+            # Pass exceptions to the Transaction User and terminate the transaction
+            self.notifyTU(e)
+            self.terminate()
+
     def ack(self, autoClean=False):
+        """Send a SIP ACK message."""
         request = self.buildRequest("ACK")
         self.sendToTransport(request, (self.remoteIP, self.remotePort))
-
         if autoClean:
             self.terminate()
 
     async def _handleRetransmissions(self, duration):
+        """Acks Invite and absorbs Non-Invite response retransmissions for the specified duration before terminating transaction."""
         try:
             async with asyncio.timeout(duration):
                 while(True):
@@ -181,12 +192,14 @@ class ClientTransaction(Transaction):
                         self.ack()
         except:
             raise
-        
         finally:
             self.terminate()
 
     def _genCallID(self):
+        """Generates and returns a suitable SIP Call ID."""
         return hex(time.time_ns())[2:] + hex(int(random.getrandbits(32)))[2:]
     
     def _genBranch(self):
+        """Generates and returns a suitable SIP branch parameter."""
+        # Calculated in accordance with loop-detection method described in Step 6 of RFC section 16.6
         return Transaction.BRANCH_MAGIC_COOKIE + hashlib.md5((self.toTag + self.fromTag + self.callID + f'SIP/2.0/UDP {self.localIP}:{self.localPort};' + str(self.sequence)).encode()).hexdigest()
