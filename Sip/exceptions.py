ffd0b006c3ed50d46df919c98c5c9beb1e474546
Dys13xic <campbell.leckie@hotmail.com>
2025-04-14 01:51:34 -0400

    Add docstrings.

--- Sip/exceptions.py
+++ Sip/exceptions.py
@@ -1,5 +1,7 @@
 class SipException(Exception):
+    """Base exception for Sip module."""
     pass
 
 class InviteError(SipException):
+    """Indicates failure to establish a dialog during an Invite request."""
     pass
