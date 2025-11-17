4c0391c64b51274eb6a4c6e81ad3081d84d0b4b1
Dys13xic <campbell.leckie@hotmail.com>
2025-11-17 00:36:59 -0500

    Make Voip a child of SessionManager, move eventHandler to UserAgent, move session management logic into SessionManager.

--- voip.py
+++ voip.py
@@ -3,9 +3,9 @@ from Sip.sip import Sip
 from Sip.transaction import Transaction
 from Sip.dialog import Dialog
 from rtp import RtpEndpoint
-from Utils.events import EventHandler
 from Utils.addressFilter import AddressFilter
 from Sip.exceptions import InviteError
+from Sip.sessionManager import SessionManager
 
 # Standard Library
 import asyncio
@@ -15,24 +15,22 @@ DEFAULT_SIP_PORT = 5060
 DEFAULT_RTP_PORT = 5004
 DEFAULT_RTCP_PORT = 5005
 
-class Voip():
+class Voip(SessionManager):
     """Manages the VoIP service."""
     def __init__(self, publicIP, sipPort=DEFAULT_SIP_PORT, rtpPort=DEFAULT_RTP_PORT, rtcpPort=DEFAULT_RTCP_PORT, allowList=[]):
+        super().__init__()
         self.sipPort: int = sipPort
         self.rtpPort: int = rtpPort
         self.rtcpPort: int = rtcpPort or rtpPort + 1
-        self.addressFilter: AddressFilter = AddressFilter(allowList)
-        self.eventHandler: EventHandler = EventHandler()
-        self.recvQueue: asyncio.Queue = asyncio.Queue()
-        self.sipEndpoint: Sip = Sip(self.recvQueue, publicIP, self.sipPort)
-        self.rtpEndpoint: RtpEndpoint = None
-        self.rtcpEndpoint: RtpEndpoint = None
-        self.activeInvite: Transaction = None
-        self.activeDialog: Dialog = None
         self.remoteRtpPort: int = None
         self.remoteRtcpPort: int = None
-        self.answerCall: asyncio.Event = asyncio.Event()
-        self.sessionStarted: asyncio.Event = asyncio.Event()
+        self.addressFilter: AddressFilter = AddressFilter(allowList)
+        self.sipEndpoint: Sip = Sip((publicIP, self.sipPort), self)
+        self.rtpEndpoint: RtpEndpoint = None
+        self.rtcpEndpoint: RtpEndpoint = None
+    
+    async def run(self):
+        await self.sipEndpoint.run()
     
     async def call(self, remoteIP):
         try:
@@ -56,37 +54,32 @@ class Voip():
         ssrc = Voip.genSSRC()
 
         loop = asyncio.get_event_loop()
-        _, endpoint = await loop.create_datagram_endpoint(
+        _, self.rtpEndpoint = await loop.create_datagram_endpoint(
         lambda: RtpEndpoint(ssrc, encrypted=False),
         local_addr=("0.0.0.0", self.rtpPort),
         remote_addr=(remoteIP, remoteRtpPort)
         )
 
-        _, ctrlEndpoint = await loop.create_datagram_endpoint(
+        _, self.rtcpEndpoint = await loop.create_datagram_endpoint(
             lambda: RtpEndpoint(ssrc, encrypted=False),
             local_addr=('0.0.0.0', self.rtcpPort),
             remote_addr=(remoteIP, remoteRtcpPort)
             )
 
-        self.rtpEndpoint = endpoint
-        self.rtcpEndpoint = ctrlEndpoint
         self.activeDialog = dialog
         self.activeInvite = None
-        self.sessionStarted.set()
+        self.sessionStart.set()
 
     def cleanup(self):
+        super().cleanup()
+
         if self.rtpEndpoint:
             self.rtpEndpoint.stop()
         if self.rtcpEndpoint:
             self.rtcpEndpoint.stop()
 
-        self.activeInvite = None
-        self.activeDialog = None
         self.rtpEndpoint, self.rtcpEndpoint = None, None
         self.remoteRtpPort, self.remoteRtcpPort = None, None
-
-        self.answerCall.clear()
-        self.sessionStarted.clear()
 
     @staticmethod
     def genSSRC():
